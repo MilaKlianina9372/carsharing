72b0e75f0ee27098f5b382545515d2478cfcaaa1
MilaKlianina <52838209+MilaKlianina@users.noreply.github.com>
2021-06-22 16:12:36 +0300

    js

---
+++ js/main.js
@@ -0,0 +1,67 @@
+$(function(){
+   
+   $('.top__slider').slick({
+      arrows: false,
+      dots: true,
+      autopay: true,
+      fade: true,
+      responsive: [
+         {
+      breakpoint: 1200,
+      settings: {
+         dots: false,
+      }
+   }
+   ]
+   });
+   
+   $('.reviews__slider').slick({
+      arrows: false,
+      dots: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+         {
+      breakpoint: 1141,
+      settings: {
+         slidesToShow: 3,
+      }
+   },
+   {
+      breakpoint: 846,
+      settings: {
+         slidesToShow: 2,
+      }
+   },
+   {
+      breakpoint: 585,
+      settings: {
+         slidesToShow: 1,
+      }
+   },
+   ]
+   });
+
+   $('.menu-btn').on('click', function(){
+      $('.menu__list').toggleClass('menu__list--active');  
+   })
+
+   //E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+
+});
